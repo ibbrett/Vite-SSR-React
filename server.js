49ec8f1153e4d412dca04a611c09d018ae454076
Suva <suva@Bretts-MacBook-Air.local>
2025-12-10 09:47:35 -1000

    Fix server

--- server.js
+++ server.js
@@ -25,44 +25,42 @@ async function createServer() {
   // middlewares). The following is valid even after restarts.
   app.use(vite.middlewares);
 
-  app.use("*all", async (req, res) => {
-    app.use("*all", async (req, res, next) => {
-      const url = req.originalUrl;
+  app.use("*all", async (req, res, next) => {
+    const url = req.originalUrl;
 
-      try {
-        // 1. Read index.html
-        let template = fs.readFileSync(
-          path.resolve(__dirname, "index.html"),
-          "utf-8"
-        );
+    try {
+      // 1. Read index.html
+      let template = fs.readFileSync(
+        path.resolve(__dirname, "index.html"),
+        "utf-8"
+      );
 
-        // 2. Apply Vite HTML transforms. This injects the Vite HMR client,
-        //    and also applies HTML transforms from Vite plugins, e.g. global
-        //    preambles from @vitejs/plugin-react
-        template = await vite.transformIndexHtml(url, template);
+      // 2. Apply Vite HTML transforms. This injects the Vite HMR client,
+      //    and also applies HTML transforms from Vite plugins, e.g. global
+      //    preambles from @vitejs/plugin-react
+      template = await vite.transformIndexHtml(url, template);
 
-        // 3. Load the server entry. ssrLoadModule automatically transforms
-        //    ESM source code to be usable in Node.js! There is no bundling
-        //    required, and provides efficient invalidation similar to HMR.
-        const { render } = await vite.ssrLoadModule("/src/entry-server.jsx");
+      // 3. Load the server entry. ssrLoadModule automatically transforms
+      //    ESM source code to be usable in Node.js! There is no bundling
+      //    required, and provides efficient invalidation similar to HMR.
+      const { render } = await vite.ssrLoadModule("/src/entry-server.jsx");
 
-        // 4. render the app HTML. This assumes entry-server.js's exported
-        //     `render` function calls appropriate framework SSR APIs,
-        //    e.g. ReactDOMServer.renderToString()
-        const appHtml = await render(url);
+      // 4. render the app HTML. This assumes entry-server.js's exported
+      //     `render` function calls appropriate framework SSR APIs,
+      //    e.g. ReactDOMServer.renderToString()
+      const appHtml = await render(url);
 
-        // 5. Inject the app-rendered HTML into the template.
-        const html = template.replace(`<!--ssr-outlet-->`, () => appHtml);
+      // 5. Inject the app-rendered HTML into the template.
+      const html = template.replace(`<!--ssr-outlet-->`, () => appHtml);
 
-        // 6. Send the rendered HTML back.
-        res.status(200).set({ "Content-Type": "text/html" }).end(html);
-      } catch (e) {
-        // If an error is caught, let Vite fix the stack trace so it maps back
-        // to your actual source code.
-        vite.ssrFixStacktrace(e);
-        next(e);
-      }
-    });
+      // 6. Send the rendered HTML back.
+      res.status(200).set({ "Content-Type": "text/html" }).end(html);
+    } catch (e) {
+      // If an error is caught, let Vite fix the stack trace so it maps back
+      // to your actual source code.
+      vite.ssrFixStacktrace(e);
+      next(e);
+    }
   });
 
   app.listen(5173);
